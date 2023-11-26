83e33f9e2a84d48ace2e58b9c0d940da27721cbf
Pathu-24 <144852667+Pathu-24@users.noreply.github.com>
2023-11-25 18:22:04 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,29 +2,31 @@ import json
 import sys
 from pymongo import MongoClient
 
-from load-json import load_json
+# from load_json import load_json
 
 # Connecting to MongoDB
 client = MongoClient('localhost', 27017)
-db = client[db]
+db = client['291db']
 collection = db['tweets']
 
 
-def search_tweets(keywords: tuple, db: str):
-    
+def search_tweets(keywords):
     # Matching tweets containing all keywords
-    query = {'$and': [{'content': {'$regex': keyword, '$options': 'i'}} 
-    for keyword in keywords
-    ]}
+    query = {'$and': [{'content': {'$regex': keyword, '$options': 'i'}}
+                      for keyword in keywords
+                      ]}
 
     results = collection.find(query)
 
     # Print the id, date, content, and username for each matching tweet
-    for tweet in results:
-        print(f"ID: {tweet['id']}")
-        print(f"Date: {tweet['date']}")
-        print(f"Content: {tweet['content']}")
-        print(f"Username: {tweet['username']}")
-        print("-------------------")
 
     return results
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: <script>.py <keyword1> <keyword2> ... <keywordN>")
+        sys.exit(1)
+
+    keywords = sys.argv[1:]
+    search_tweets(keywords)
