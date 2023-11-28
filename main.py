21dd3b05bbcedb32064b38d08f01a2370d5b957a
Pathu-24 <144852667+Pathu-24@users.noreply.github.com>
2023-11-27 17:31:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-import functions.py
-import load_json.py
+import functions
+import load_json
+
 
 def main_menu():
     print("Welcome to your twitchat homepage! What would you like to do?: ")
@@ -19,16 +20,18 @@ def main_menu():
             print("Invalid option selected! Please enter a number between 1 and 5.")
     return int(option)
 
+
 def correct_input(usr_input: str, a: int, b: int):
     uinput = 0
     if usr_input.isdigit():
         uinput = int(usr_input)
     else:
         return 0
-    
+
     if (uinput >= a and uinput <= b) or uinput == 0:
         return 1
     return 0
+
 
 def display_tweets(keywords: tuple):
     tweets = functions.search_tweets(keywords)
@@ -36,16 +39,17 @@ def display_tweets(keywords: tuple):
     i = 0
     for tweet in tweets:
         i += 1
-        print(f'{i}. id: {tweet["id"]}, Date: {tweet["date"]}, Username: {tweet["user"]["username"]}\nTweet Content: {tweet["content"]}')
-    
-    #If there is at least one tweet that matches the keywords, give the user the option to select one
+        print(
+            f'{i}. id: {tweet["id"]}, Date: {tweet["date"]}, Username: {tweet["user"]["username"]}\nTweet Content: {tweet["content"]}')
+
+    # If there is at least one tweet that matches the keywords, give the user the option to select one
     if i != 0:
         answer = input("Select a tweet to see more info about it or enter 0 to return to main menu: ")
         while correct_input(answer, 1, i) != 1:
             answer = input(f"Please enter a number between 0 and {i}: ")
 
         answer = int(answer)
-        #Print all fields
+        # Print all fields
         if answer != 0:
             j = 1
             for tweet in tweets:
@@ -59,6 +63,7 @@ def display_tweets(keywords: tuple):
         print("No such tweets")
     return
 
+
 def display_users(keyword: str):
     users = functions.search_users(keyword)
     users = list(users)
@@ -66,15 +71,15 @@ def display_users(keyword: str):
     for user in users:
         i += 1
         print(f'{i}. Username: {user["username"]}, Display name: {user["displayname"]}, Location: {user["location"]}')
-    
-    #If there is at least one user that matches the keyword, give the option to select them
+
+    # If there is at least one user that matches the keyword, give the option to select them
     if i != 0:
         answer = input("Select a user to see more info on them or enter 0 to return to main menu: ")
         while correct_input(answer, 1, i) != 1:
             answer = input(f"Please enter a number between 0 and {i}: ")
 
         answer = int(answer)
-        #Print all fields
+        # Print all fields
         if answer != 0:
             for j, user in enumerate(users, start=1):
                 if answer == j:
@@ -87,22 +92,24 @@ def display_users(keyword: str):
 
     return
 
+
 def display_top_tweets(n: int, count: str):
     tweets = functions.top_tweets(n, count)
     tweets = list(tweets)
     i = 0
     for tweet in tweets:
         i += 1
-        print(f'{i}. ID: {tweet["id"]}, Date: {tweet["date"]}, Username: {tweet["username"]}\nTweet Content: {tweet["content"]}')
+        print(
+            f'{i}. ID: {tweet["id"]}, Date: {tweet["date"]}, Username: {tweet["username"]}\nTweet Content: {tweet["content"]}')
 
-    if i != 0: 
+    if i != 0:
         answer = input("Select a tweet to see more info or enter 0 to go back to the main screen: ")
         while (correct_input(answer, 1, i) != 1):
             print("Invalid input")
             answer = input("Select a tweet to see more info or enter 0 to go back to the main screen: ")
 
         answer = int(answer)
-        #Print all fields
+        # Print all fields
         if answer != 0:
             for j, tweet in enumerate(tweets, start=1):
                 if answer == j:
@@ -112,8 +119,9 @@ def display_top_tweets(n: int, count: str):
                     return
     else:
         print("No such tweets")
-    
+
     return
+
 
 def display_top_users(n: int):
     users = functions.top_users(n)
@@ -126,9 +134,9 @@ def display_top_users(n: int):
     while (correct_input(answer, 1, i) != 1):
         print("Invalid input")
         answer = input("Select a user to see more info or enter 0 to go back to the main screen: ")
-    
+
     answer = int(answer)
-    #Print all fields
+    # Print all fields
     if answer != 0:
         for j, user in enumerate(users, start=1):
             if answer == j:
@@ -138,8 +146,9 @@ def display_top_users(n: int):
                 return
     else:
         print("No such user")
-    
+
     return
+
 
 def main():
     if len(sys.argv) != 3:
@@ -147,8 +156,7 @@ def main():
         sys.exit(1)
 
     json_file, port = sys.argv[1], int(sys.argv[2])
-    load_json(json_file, port)
-
+    load_json.loadjson(json_file, port)
 
     command = main_menu()
     while (command):
@@ -180,7 +188,6 @@ def main():
             count = input("Enter r, l, or q for retweet, like, or quote counts respectively")
             while count.islower() not in ["r", "l", "q"]:
                 count = input("Enter r, l, or q for retweet, like, or quote counts respectively")
-            
 
             # Display the tweets that match at least one of the keywords
             display_top_tweets(n, count)
@@ -204,6 +211,7 @@ def main():
         command = main_menu()
     print("Exiting...\n")
     return 0
+
 
 if __name__ == "__main__":
     main()
